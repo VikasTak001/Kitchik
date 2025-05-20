b4822b60d216c67076973aef7dd888a3aa988c90
VikasTak001 <vikastak0011@gmail.com>
2025-05-20 08:26:30 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import MainContext from './MainContext'
+import MainContext from './MainContext';
 import Layout from './Website/Layout';
 import Home from './Website/Pages/Home';
 
 export default function App() {
-  const routes = createBrowserRouter([
-    {
-      path:"/",
-      element:<Layout/>,
-      children:[
-        {
-          path:"/",
-          element:<Home/>
-        } 
-      ]
-    }
-  ])
+  const routes = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "/",
+            element: <Home />
+          }
+        ]
+      }
+    ],
+    { basename: "/Kitchik" }  // <-- set your GitHub repo name here
+  );
+
   return (
     <>
       <MainContext>
         <RouterProvider router={routes} />
-      </MainContext> 
+      </MainContext>
     </>
-  )
-} 
+  );
+}
